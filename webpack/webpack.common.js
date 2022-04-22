2b124b68ce366c474737e84838866961bc3947e8
keman <k754708625@gmail.com>
2022-04-22 22:50:05 +0800

    fix: 修复 npm7+ 运行报错

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -9,32 +9,28 @@ const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 const resolve = dir => path.resolve(__dirname, dir);
 
 // 命令行/环境变量
-const webpackEnv = require('./utils/env-analyzer');
-
-const {
-  devMode,
-} = webpackEnv;
+const devMode = process.env.NODE_ENV !== 'production';
 
 const plugins = [];
 
 // 添加分析插件
-if (webpackEnv.envParams.report !== undefined) {
+/* if (webpackEnv.envParams.report !== undefined) {
   plugins.push(new BundleAnalyzerPlugin());
-}
+} */
 
 // Dashboard 插件
-if (webpackEnv.envParams.log !== undefined) {
+/* if (webpackEnv.envParams.log !== undefined) {
   const Dashboard = require('webpack-dashboard');
   const DashboardPlugin = require('webpack-dashboard/plugin');
   const dashboard = new Dashboard();
 
   plugins.push(new DashboardPlugin(dashboard.setData));
-}
+} */
 
 const cssloaders = [
   'css-loader',
@@ -161,5 +157,5 @@ const webpackConfig = {
 
 module.exports = {
   webpackConfig,
-  webpackEnv,
+  devMode,
 };
